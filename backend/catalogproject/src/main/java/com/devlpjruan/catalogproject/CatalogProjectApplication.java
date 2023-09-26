1dfdeb8fb6eb67eee92dc669a187687158cb898b
Ruan Matheus <juanmatheus360@gmail.com>
2023-09-26 17:22:18 -0400

    Project created

---
+++ backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/CatalogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devlpjruan.catalogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogProjectApplication.class, args);
+	}
+
+}
---
+++ backend/catalogproject/src/test/java/com/devlpjruan/catalogproject/CatalogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlpjruan.catalogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
