617c4a38fd5e6507fbc5e408f32bb0db85877937
Ruan Matheus <juanmatheus360@gmail.com>
2023-09-28 23:28:09 -0400

    Auditing data

--- backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/entities/Category.java
+++ backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/entities/Category.java
@@ -1,12 +1,16 @@
 package com.devlpjruan.catalogproject.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +22,10 @@ public class Category implements Serializable{
 	@Id
 	private Long id;
 	private String name;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Category( ) {
 		 
@@ -38,6 +46,21 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -53,7 +76,4 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
