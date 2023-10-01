7b4ae4cfe69a2990ce029f89d3c9b46b20e9bf50
Ruan Matheus <juanmatheus360@gmail.com>
2023-10-01 12:27:32 -0400

    Refactor pageable

--- backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/services/CategoryService.java
+++ backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/services/CategoryService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,8 +22,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public Page<CategoryDTO> findAll(PageRequest pageRequest){
-		Page<Category> list = repository.findAll(pageRequest);
+	public Page<CategoryDTO> findAll(Pageable pageable){
+		Page<Category> list = repository.findAll(pageable);
 		return list.map(x -> new CategoryDTO(x));
 	}
 
--- backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/services/ProductService.java
+++ backend/catalogproject/src/main/java/com/devlpjruan/catalogproject/services/ProductService.java
@@ -5,15 +5,17 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devlpjruan.catalogproject.DTO.CategoryDTO;
 import com.devlpjruan.catalogproject.DTO.ProductDTO;
 import com.devlpjruan.catalogproject.entities.Category;
 import com.devlpjruan.catalogproject.entities.Product;
 import com.devlpjruan.catalogproject.exceptions.DatabaseException;
 import com.devlpjruan.catalogproject.exceptions.ResourceNotFoundException;
+import com.devlpjruan.catalogproject.repositories.CategoryRepository;
 import com.devlpjruan.catalogproject.repositories.ProductRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -23,9 +25,12 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Autowired
+	private CategoryRepository catRepository;
+	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAll(PageRequest pageRequest){
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAll(Pageable pageable){
+		Page<Product> list = repository.findAll(pageable);
 		return list.map(x -> new ProductDTO(x));
 		
 	}
@@ -33,17 +38,13 @@ public class ProductService {
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(""));
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found."));
 		return new ProductDTO(entity, entity.getCategories());
 	}
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 			Product entity = new Product();
-			entity.setName(dto.getName());
-			entity.setDescription(dto.getDescription());
-			entity.setPrice(dto.getPrice());
-			entity.setImgUrl(dto.getImgUrl());
-			entity= repository.save(entity);
+			dtoToEntity(dto, entity);
 			return new ProductDTO(entity);
 	}
 	
@@ -51,11 +52,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 		Product entity = repository.getReferenceById(id);
-		entity.setName(dto.getName());
-		entity.setDescription(dto.getDescription());
-		entity.setPrice(dto.getPrice());
-		entity.setImgUrl(dto.getImgUrl());
-		entity= repository.save(entity);
+		dtoToEntity(dto, entity);
 		return new ProductDTO(entity);
 		}catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found "+ id);
@@ -73,5 +70,18 @@ public class ProductService {
 	        	throw new DatabaseException("Falha de integridade referencial");
 	   	}
 	}
-	
+	private Product dtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories() ) {
+			Category categoty = catRepository.getReferenceById(catDto.getId());
+			entity.getCategories().add(categoty);
+			
+		}
+		entity= repository.save(entity);
+		return entity;
+	}
 }
